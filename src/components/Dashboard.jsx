c0c11f87feba829fdf99999a0234cfd6e0d2667c
Ondřej Diepolt - notebook <diepoond@cvut.cz>
2026-03-04 09:46:38 +0100

    Pie graphs with recharts added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,9 @@
 import { useBudget } from '../context/BudgetContext'
 import { useBudgetStats } from '../hooks/useBudgetStats'
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts'
+
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A28DFF', '#FF6B6B']
 
 function Dashboard() {
     const { transactions } = useBudget()
@@ -8,11 +12,43 @@ function Dashboard() {
    console.log('stats:', stats)
 
   return (
-    <div>
+    <div className="dashboard">
       <h2>Dashboard</h2>
-      <p>Income: {stats.totalIncome} Kč</p>
-      <p>Expense: {stats.totalExpenses} Kč</p>
-      <p>Balance: {stats.balance} Kč</p>
+
+      {/* <div>
+        <p>Income: {stats.totalIncome} Kč</p>
+        <p>Expense: {stats.totalExpenses} Kč</p>
+        <p>Balance: {stats.balance} Kč</p>
+      </div> */}
+
+        <div className="dashboard-charts">
+            <div>
+                <h3>Výdaje podle kategorie</h3>
+                <PieChart width={400} height={300}>
+                    <Pie data={stats.expenseBreakdown} dataKey="amount" nameKey="category">
+                    {stats.expenseBreakdown.map((_, index) => (
+                        <Cell key={index} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                    </Pie>
+                    <Tooltip formatter={(value) => `${value} Kč`} />
+                    <Legend />
+                </PieChart>
+            </div>
+            
+            <div>
+                <h3>Příjmy podle kategorie</h3>
+                <PieChart width={400} height={300}>
+                    <Pie data={stats.incomeBreakdown} dataKey="amount" nameKey="category">
+                    {stats.incomeBreakdown.map((_, index) => (
+                        <Cell key={index} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                    </Pie>
+                    <Tooltip formatter={(value) => `${value} Kč`} />
+                    <Legend />
+                </PieChart>
+            </div>
+
+        </div>
     </div>
   )
 }
